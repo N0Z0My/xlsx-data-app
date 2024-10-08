cc3b539d0ea9e1c2b3f98cd5d5253f57246b69a6
N0Z0My <131774194+N0Z0My@users.noreply.github.com>
2024-10-08 04:57:57 +0000

    this is first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,25 @@
 import streamlit as st
+import pandas as pd
+import openpyxl
+import random
 
+
+st.set_page_config(page_title='my new app')
 st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+df = pd.read_excel('list_kaigai.xlsx', sheet_name='sheet1',index_col=0)
+
+with st.expander('df', expanded=False):
+    st.table(df)
+
+st.markdown('##### 質問')
+
+len_df = len(df)
+ #乱数取得 
+num = random.randint(0,len_df-1)
+ #dfから行を抽出  series
+s_selected = df.loc[num]
+ #seriesから値の抽出 
+val = s_selected.loc['問題']
+
+st.markdown(f'## {val}') 
