989f2dfeebed4b357f58378df974c6600448fd5e
N0Z0My <131774194+N0Z0My@users.noreply.github.com>
2024-10-10 06:59:50 +0000

    this is update for radio

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,22 +19,14 @@ s_selected = df.loc[num]
 # seriesから値の抽出 
 question = s_selected.loc['問題']
 
+optionA = s_selected.loc['選択肢A']
+optionB = s_selected.loc['選択肢B']
+optionC = s_selected.loc['選択肢C']
+
+
 st.markdown(f'## {question}')
 
-# D, E, F列の値を取得
-options = [s_selected.loc['D'], s_selected.loc['E'], s_selected.loc['F']]
-
-# ラジオボタンを表示
-selected_option = st.radio("選択肢", options, key="quiz_options")
-
-# 回答を確認するボタン
-if st.button("回答を確認"):
-    correct_answer = s_selected.loc['正解']
-    if selected_option == correct_answer:
-        st.success("正解です！")
-    else:
-        st.error(f"不正解です。正解は {correct_answer} でした。")
-
-# 次の問題ボタン
-if st.button("次の問題"):
-    st.experimental_rerun()
+stock = st.radio(label='回答を選択してください',
+                 options=({optionA}, {optionB}, {optionC}),
+                 index=0,
+                 horizontal=True,)
